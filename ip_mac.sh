eeb29064b7d61fe8f34873f1996a8e93a5653cb1
RETRO SPOON <the.one.red.cockadoo@gmail.com>
2019-09-02 00:00:54 -0400

    Update ip_mac.sh

--- ip_mac.sh
+++ ip_mac.sh
@@ -4,14 +4,25 @@
 #		   			for desired interface
 #		     		Does not persist over reboot
 #usage		 ./ip_mac.sh
-#notes		 Install macchanger to use
-echo Enter desired interface
-read int
-sudo ifconfig $int down
-sudo macchanger -b -r $int
-sudo ifconfig $int
-sudo dhclient -r $int
-sudo dhclient $int
-clear
-sudo ifconfig $int
-sudo macchanger -s $int
+#notes		 Requires macchanger and whiptail to use
+int=$(whiptail --title "Interface" --inputbox "Enter the desired interface" 10 60 3>&1 1>&2 2>&3)
+
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+	clear
+	sudo ifconfig $int down
+	sudo macchanger -b -r $int
+	clear
+	sudo ifconfig $int
+	clear
+	sudo dhclient -r $int
+	clear
+	sudo dhclient $int
+	clear
+	sudo ifconfig $int
+	sudo macchanger -s $int
+
+else
+	clear
+    echo "You chose Cancel."
+fi
