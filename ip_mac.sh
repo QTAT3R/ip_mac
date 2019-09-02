47e0eefe195e7f44e895b01d220ee475252c2c7b
RETRO SPOON <the.one.red.cockadoo@gmail.com>
2019-09-01 22:33:31 -0400

    Update ip_mac.sh

--- ip_mac.sh
+++ ip_mac.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-#author		 github.com/QTAT3R
+#author		     github.com/retro-spoon
 #description	 Script to automate changing mac and ip address
-#		              for interface wlan0
-#		                Does not persist over reboot
+#		   			for desired interface
+#		     		Does not persist over reboot
 #usage		 ./ip_mac.sh
 #notes		 Install macchanger to use
-sudo ifconfig wlan0 down
-sudo macchanger -b -r wlan0
-sudo ifconfig wlan0 up
-sudo dhclient -r wlan0
-sudo dhclient wlan0
+echo Enter desired interface
+read int
+sudo ifconfig $int down
+sudo macchanger -b -r $int
+sudo ifconfig $int
+sudo dhclient -r $int
+sudo dhclient $int
 clear
-sudo ifconfig wlan0
-sudo macchanger -s wlan0
+sudo ifconfig $int
+sudo macchanger -s $int
