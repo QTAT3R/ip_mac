cb2184d115488cfab8c9d5d8dd37623478745bcf
RETRO SPOON <the.one.red.cockadoo@gmail.com>
2019-09-01 21:51:58 -0400

    Update ip_mac.sh

--- ip_mac.sh
+++ ip_mac.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #author		 github.com/QTAT3R
-#description	 Script to auto change mac and ip address
-#		   for interface wlan0
-#		     Does not persist over reboot
+#description	 Script to automate changing mac and ip address
+#		              for interface wlan0
+#		                Does not persist over reboot
 #usage		 ./ip_mac.sh
 #notes		 Install macchanger to use
 sudo ifconfig wlan0 down
